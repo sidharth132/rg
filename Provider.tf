53051a3f5375c8f88ba3b5dcfd53807c4be7949f
sidharth132 <55968842+sidharth132@users.noreply.github.com>
2026-09-16 22:54:44 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+
+  # backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "5ef4ee5d-6bd4-4c27-acc2-af8816fed413"
+}
 
